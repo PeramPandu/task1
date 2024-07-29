6e06fe2a6efc34f50665c1e8bf498c58fff8d4b6
PeramPandu <perampandu02@gmail.com>
2024-07-29 20:50:43 +0530

    task2

---
+++ materialui/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import FormWithYup from "./components/FormWithYup";
+
+
+function App() {
+  return (
+    <div>
+      
+      <FormWithYup />
+    </div>
+  );
+}
+
+export default App;
---
+++ materialui/src/components/FormWithYup.jsx
@@ -0,0 +1,202 @@
+import {useState} from "react";
+import * as Yup from "yup";
+
+const FormWithYup = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    confirmPassword: "",
+    age: "",
+    gender: "",
+    
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const validationSchema = Yup.object({
+    firstName: Yup.string().required("First Name is Required"),
+    lastName: Yup.string().required("Last Name is Required"),
+    email: Yup.string()
+      .required("Email is Required")
+      .email("Invalid email format"),
+    phoneNumber: Yup.string()
+      .matches(/^\d{10}$/, "Phone Number must be 10 digits")
+      .required(),
+    password: Yup.string()
+      .required("Password is required")
+      .min(8, "Password must be at least 8 characters")
+      .matches(
+        /[!@#$%^&*(),.?":{}|<>]/,
+        "Password must contain at least one symbol"
+      )
+      .matches(/[0-9]/, "Password must contain at least one number")
+      .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
+      .matches(/[a-z]/, "Password must contain at least one lowercase letter"),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref("password")], "Passwords must match")
+      .required("Confirm password is required"),
+    age: Yup.number()
+      .typeError("Age must be a number")
+      .min(18, "You must be at least 18 years old")
+      .max(100, "You cannot be older than 100 years")
+      .required("Age is required"),
+    gender: Yup.string().required("Gender is required"),
+   
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const nonParsed = {
+      firstName: "Piyush",
+      lastName: "Agarwal",
+      email: "piyush@example.com",
+      phoneNumber: "1231234218",
+      password: "123456Qq*",
+      confirmPassword: "123456Qq*",
+      age: "18",
+      gender: "male",
+      
+    };
+
+    const parsedUser = validationSchema.cast(nonParsed);
+
+    console.log(nonParsed, parsedUser);
+
+    try {
+      await validationSchema.validate(formData, {abortEarly: false});
+      console.log("Form Submitted", formData);
+    } catch (error) {
+      const newErrors = {};
+
+      error.inner.forEach((err) => {
+        newErrors[err.path] = err.message;
+      });
+
+      setErrors(newErrors);
+    }
+  };
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleCheckboxChange = (e) => {
+    const {name, checked} = e.target;
+   
+
+   
+  };
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <div>
+        <label>First Name:</label>
+        <input
+          type="text"
+          name="firstName"
+          value={formData.firstName}
+          placeholder="Enter your first name"
+          onChange={handleChange}
+        />
+        {errors.firstName && <div className="error">{errors.firstName}</div>}
+      </div>
+      <div>
+        <label>Last Name:</label>
+        <input
+          type="text"
+          name="lastName"
+          value={formData.lastName}
+          placeholder="Enter your last name"
+          onChange={handleChange}
+        />
+        {errors.lastName && <div className="error">{errors.lastName}</div>}
+      </div>
+      <div>
+        <label>Email:</label>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          placeholder="Enter your email"
+          onChange={handleChange}
+        />
+        {errors.email && <div className="error">{errors.email}</div>}
+      </div>
+      <div>
+        <label>Phone Number:</label>
+        <input
+          type="text"
+          name="phoneNumber"
+          value={formData.phoneNumber}
+          placeholder="Enter your phone number"
+          onChange={handleChange}
+        />
+        {errors.phoneNumber && (
+          <div className="error">{errors.phoneNumber}</div>
+        )}
+      </div>
+      <div>
+        <label>Password:</label>
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          placeholder="Enter your password"
+          onChange={handleChange}
+        />
+        {errors.password && <div className="error">{errors.password}</div>}
+      </div>
+      <div>
+        <label>Confirm Password:</label>
+        <input
+          type="password"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          placeholder="Confirm your password"
+          onChange={handleChange}
+        />
+        {errors.confirmPassword && (
+          <div className="error">{errors.confirmPassword}</div>
+        )}
+      </div>
+      <div>
+        <label>Age:</label>
+        <input
+          type="number"
+          name="age"
+          value={formData.age}
+          placeholder="Enter your age"
+          onChange={handleChange}
+        />
+        {errors.age && <div className="error">{errors.age}</div>}
+      </div>
+
+      <div>
+        <label>Gender:</label>
+        <select name="gender" value={formData.gender} onChange={handleChange}>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="other">Other</option>
+        </select>
+        {errors.gender && <div className="error">{errors.gender}</div>}
+      </div>
+
+      
+       
+       
+      
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default FormWithYup;
